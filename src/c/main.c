d129d689714f74c36fee4371b6dece796e6d4bf4
Bartłomiej Kumor <aargh1@gmail.com>
2015-03-11 23:16:06 +0100

    Changes to include new project requirements
    TO-DO: add latency/time/badwidth measurement, refactor(!), add main loop
    for iterations, test if the code works in Linux env, change file-save
    path

--- src/c/main.c
+++ src/c/main.c
@@ -6,30 +6,11 @@
 #include <time.h>
 #include <sys/time.h>
 
-void parseOptions(int *argc, char *argv[])
-{
-    if(*argc==0)
-    {
-        printf("The all-knowing programmer predicted this error\n");
-        exit(666);
-    }
-    if(*argc==1)
-    {
-        printf("No options provided, closing\n");
-        exit(1);
-    }else if(*argc!=3)
-    {
-        printf("The number of input args must be 3!\n");
-        exit(2);
-    }
-}
-
 int main(int argc, char* argv[])
 {
-    parseOptions(&argc, argv);
-
-    int sizeOfData = atoi(argv[2]);
-    int sendMode = atoi(argv[1]);
+    int sizeOfData[16] = {1000, 5000, 10000,20000,30000,50000,100000,
+              200000,300000,500000,1000000,2000000,3000000,
+              5000000,6000000,10000000};
 
     MPI_Init(&argc, &argv);
     int world_rank;
@@ -40,6 +21,10 @@ int main(int argc, char* argv[])
     int arrayToSend[sizeOfData];
     int arrayReceivedStd[sizeOfData];
     int arrayReceivedSynch[sizeOfData];
+    int arrayFromSecondProcess[sizeOfData];
+
+    int numberOfIterations = 0;
+
     int i = 0;
     for(i = 0; i<sizeOfData; i++)
     {
@@ -53,32 +38,53 @@ int main(int argc, char* argv[])
     {
       arrayReceivedSynch[i] = 0;
     }
+    for(i = 0; i<sizeOfData; i++)
+    {
+      arrayFromSecondProcess[i] = 0;
+    }
     MPI_Barrier(MPI_COMM_WORLD);
     if (world_size < 2)
     {
         fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
-    double t1, t2, timeDiff = 0;
     if (world_rank == 0)
     {
+
         MPI_Send(&arrayToSend, sizeOfData, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        //MPI_Ssend(&arrayToSend, sizeOfData, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&arrayFromSecondProcess, sizeOfData, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        for(i = 0; i<sizeOfData; i++)
+        {
+            printf("%i", arrayFromSecondProcess[i]);
+        }
+        printf("\n");
+        MPI_Ssend(&arrayToSend, sizeOfData, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&arrayFromSecondProcess, sizeOfData, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        for(i = 0; i<sizeOfData; i++)
+        {
+            printf("%i", arrayFromSecondProcess[i]);
+        }
+        printf("\n");
     } else if (world_rank == 1)
     {
-        //FILE * fileOptions = fopen("D://results.txt", "w"); ///fix for Gandalf paths
+        /FILE * fileOptions = fopen("D://results.txt", "w"); ///fix for Gandalf paths
         //printf("Preparing to receive data\n");
         MPI_Recv(&arrayReceivedStd, sizeOfData, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        //MPI_Recv(&arrayReceivedSynch, sizeOfData, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&arrayReceivedStd, sizeOfData, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(&arrayReceivedSynch, sizeOfData, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Ssend(&arrayToSend, sizeOfData, MPI_INT, 0, 0, MPI_COMM_WORLD);
         printf("Received\n");
         for(i = 0; i<sizeOfData; i++)
         {
             printf("%i", arrayReceivedStd[i]);
         }
+        printf("\n");
         for(i = 0; i<sizeOfData; i++)
         {
             printf("%i", arrayReceivedSynch[i]);
         }
+        printf("\n");
         printf("Time: %d\n", timeDiff);
     }
 
